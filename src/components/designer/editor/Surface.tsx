418fa6f1d20b8f03681dfd02e8151354cb0f49e5
Kevin Mistry <digitalmystery@protonmail.com>
2024-09-18 12:13:34 -0400

    adjusted surface and gave it a solid border with shadow;

--- src/components/designer/editor/Surface.tsx
+++ src/components/designer/editor/Surface.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useNode } from '@craftjs/core';
 
-const Surface: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+interface SurfaceProps {
+  isExportMode?: boolean; // Flag to remove borders/shadows during export
+}
+
+const Surface: React.FC<React.PropsWithChildren<SurfaceProps>> = ({ children, isExportMode = false }) => {
   const {
     connectors: { connect, drag },
   } = useNode();
@@ -9,8 +13,14 @@ const Surface: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
   return (
     <div
       ref={(ref) => connect(drag(ref as HTMLElement))}
-      className="w-full h-full border border-dashed border-gray-300 flex flex-col items-start justify-start"
-      style={{ minHeight: '500px', backgroundColor: "#ffffff" }} // White background for surface
+      className={`relative w-full h-full flex flex-col items-start justify-start ${
+        !isExportMode ? 'border border-gray-300 shadow-2xl' : ''
+      }`}
+      style={{
+        minHeight: '500px',
+        backgroundColor: "#ffffff",
+        boxShadow: !isExportMode ? '0 10px 20px rgba(0, 0, 0, 0.1), 0 0px 20px rgba(0, 0, 0, 0.1)' : '',
+      }}
     >
       {children}
     </div>
